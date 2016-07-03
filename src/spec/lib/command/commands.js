3fdab1464c92ea49ccac0b321f7b2c805da3f7cc
Shuichiro Kamiya <notsunohito@gmail.com>
2016-07-03 14:31:36 +0900

    Add spec:lib/command/constructor

--- src/spec/lib/command/commands.js
+++ src/spec/lib/command/commands.js
@@ -1,5 +1,5 @@
-import {expect}   from 'chai';
-import Commands from '../../../lib/command/commands';
+import {expect} from 'chai';
+import Commands, {Goto} from '../../../lib/command/commands';
 
 
 describe('Commands', ()=> {
@@ -17,18 +17,16 @@ describe('Commands', ()=> {
 describe('Command', ()=> {
 
     context('Goto', ()=> {
-        const Goto = Commands.findByName('goto');
-        const goto = new Goto(['url']);
 
         describe('name', ()=> {
             it('class名の小文字を返す', ()=> {
-                expect(goto.name).to.equal('goto');
+                expect(new Goto(['url']).name).to.equal('goto');
             });
         });
 
         describe('toJSON', ()=> {
             it('{ name:string, args:any[] }を返す', ()=> {
-                const {name, args} = goto.toJSON();
+                const {name, args} = new Goto(['url']).toJSON();
                 expect(typeof name).to.be.equal('string');
                 expect(args).to.be.an.instanceof(Array);
             });
